a32a6431ea8eccc2810af32c4204bd410e193a92
Santiago Ortiz Guevara <santiagortizgue@MacBook-Pro-de-Santiago.local>
2021-08-06 22:04:39 -0500

    added dependencies

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+/*
+const App = () => {
+
+    render (
+        <div className="App">
+
+        </div>
+    );
+}
+*/
+
+const App = () => <h1>Hola Mundo</h1>
+
+export default App;
